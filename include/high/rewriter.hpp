7d27b46b745e764a8ca1a1d36e868038017f8260
Anfsity <186920685+anfsity@users.noreply.github.com>
2026-05-21 11:41:52 +0800

    feat: add high ir rewriter

---
+++ include/high/rewriter.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "ir.hpp"
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+
+namespace exodus::high_ir {
+
+class IRRewriter {
+  std::unordered_set<Op *> to_erase;
+
+public:
+  void replaceAllUsesWith(Value *old_val, Value *new_val) {
+    if (!old_val || !new_val || old_val == new_val)
+      return;
+
+    std::vector<Op *> users_copy(old_val->users.begin(), old_val->users.end());
+
+    for (auto *user : users_copy) {
+      for (auto &operand : user->operands) {
+        if (operand == old_val) {
+          operand = new_val;
+          new_val->addUse(user);
+        }
+      }
+    }
+    old_val->users.clear();
+  }
+
+  void replaceOp(Op *old_op, Value *new_val) {
+    if (old_op->result) {
+      replaceAllUsesWith(old_op->result, new_val);
+    }
+    eraseOp(old_op);
+  }
+
+  void eraseOp(Op *op) {
+    if (!op)
+      return;
+    for (auto *operand : op->operands) {
+      if (operand) {
+        operand->rmUse(op);
+      }
+    }
+    to_erase.insert(op);
+  }
+
+  void finalize(Region &r) {
+
+    auto it = r.begin();
+
+    while (it != r.end()) {
+      Op *op = *it;
+      if (to_erase.count(op)) {
+        it = r.erase(it);
+
+      } else {
+        if (op->code == OpCode::If) {
+          auto &p = std::get<IfPayload>(op->payload);
+          finalize(*p.then_region);
+          if (p.else_region)
+            finalize(*p.else_region);
+
+        } else if (op->code == OpCode::While) {
+          auto &p = std::get<WhilePayload>(op->payload);
+          finalize(*p.cond_region);
+          finalize(*p.loop_region);
+        }
+        ++it;
+      }
+    }
+  }
+
+  void finalize(Function &f) { finalize(f.body); }
+
+  void clear() { to_erase.clear(); }
+};
+
+} // namespace exodus::high_ir
